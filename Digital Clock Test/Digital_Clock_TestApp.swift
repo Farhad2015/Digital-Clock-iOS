3072b2d8c609d8099d1ed53451df1af5b6579000
Mahmudur Rahman <mahmud@Mahmudurs-Mac-mini.local>
2022-04-09 12:48:55 +0600

    Initial Project

---
+++ Digital Clock Test/Digital_Clock_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Digital_Clock_TestApp.swift
+//  Digital Clock Test
+//
+//  Created by Mahmudur Rahman on 9/4/22.
+//
+
+import SwiftUI
+
+@main
+struct Digital_Clock_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
