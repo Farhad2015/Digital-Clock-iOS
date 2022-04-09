7ab5fe94200ef0d8cbd65852268b3da99f62fc1e
Mahmudur Rahman <mahmud@Mahmudurs-Mac-mini.local>
2022-04-09 13:38:46 +0600

    digital clock functionality added

--- Digital Clock Test/ContentView.swift
+++ Digital Clock Test/ContentView.swift
@@ -8,9 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var date = Date()
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            VStack {
+                Text("\(timeString(date: date))").font(.system(size: 24)).foregroundColor(.blue).fontWeight(.bold).padding([.leading, .trailing], 20)
+                    .onAppear {
+                        let _ = self.updateTimer
+                    }
+                Text("Good Afternoon.").font(.subheadline).foregroundColor(.black)
+            }
+        }
+    }
+    
+    var timeFormat: DateFormatter{
+        let formatter = DateFormatter()
+        formatter.dateFormat = "hh:mm:ss a"
+        return formatter
+    }
+    
+    func timeString(date: Date) -> String {
+        let time = timeFormat.string(from: date)
+        return time
+    }
+    
+    var updateTimer: Timer{
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { tmr in
+            self.date = Date()
+        }
     }
 }
 
